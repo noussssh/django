86c50b4ed1826d4d30733085e16f00c5cba9375f
Noushad Moidunny <noussh@Noushads-MacBook-Pro.local>
2023-11-29 17:32:46 +0530

    changed the static details in settings

--- globalnetwork/settings.py
+++ globalnetwork/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*5wgpto93$482_km!)p6ujx#qyp9c2y)@z4a7**4(mjnbratg)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
